f4348d38a5b800d83ed3518021e85003b93f180f
François LAVAZEC <francois.lavazec@gmail.com>
2015-01-28 16:13:25 +0100

    version avec controle plus strict de la saisie

--- README.txt
+++ README.txt
@@ -11,23 +11,40 @@ main.js  // le code javascript associé au projet
 
 Pour le reste on utilise twitter bootstrap et Jquery.
 
+
 Commit 1 : Commit niveau 0
 
 uniquement README.txt on a vérifié que les commandes git étaient bien paramétrées
+
 
 Commit 2 : Squelette du projet
 
 Les trois fichiers sont créés.
 Le programme ne fait qu'afficher le titre et les fenetres de gauche et droite avec un fond spécifique.
+La fenetre de droite est dimensionnée par défaut à 300px de hauteur.
+
 
 Commit 3 : version de base
 
 fenetre de gauche : 
 - présentation améliorée du titre
 - ajout d'un champ de saisie
+- ajout d'une div pour afficher les messages d'erreur
 - ajout d'un bouton pour le lancement de l'affichage
 
 fenetre de droite:
 - présentation améliorée du titre
 - au clic du bouton de la fenetre de gauche une fonction javascript est lancée: 
 si le chiffre est inférieur ou égal à 30 et positif l'affichage se fait dans la div avec l'id latable
+
+
+Commit 4 : version avec controle plus strict de la saisie
+
+Le test de la valeur rentrée est renforcé en interdisant les nombres à virgule.
+Des commentaires sont rajoutés
+
+
+
+Commit 5 : version avec affichage amélioré
+
+Il s'agit du correctif pour le redimensionnement dynamique de la fenetre de droite
--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 
+// fonction vérifiant que la variable est bien un entier
+function isInt(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
+
 function affiche(i)
 {
-	if (i>0 && i<=30) // test simple de validité
+	if (i>0 && i<=30 && isInt(i)) // test simple de validité
 
 	{
+		i=parseInt(i,10);
 		$("#erreur").empty(); // enlever message d'erreur
+
+		
+
 		var montab=$('#latable');
-		montab.empty();
-		montab.append('<ul>');
+
+		montab.empty(); // effacer l'ancienne table
+
+		montab.append('<ul>'); // début de la liste
 		for (var j = 0; j <= i; j++) 
 		{
-			montab.append('<li>'+j+" X "+i+' = '+i*j+'</li>');
+			montab.append('<li>'+j+" X "+i+' = '+i*j+'</li>'); 
+			// génération d'une ligne <li>j X i = (i*j) </li>
 		};
-		montab.append('</ul>');
+
+		montab.append('</ul>'); // fin de la liste
 
 	}
 	else 
@@ -24,8 +40,10 @@ function affiche(i)
 
 
 $(document).ready(function(){
-	$('.panneau_droit').css('height','300px');
+	// on assigne la propriété hauteur 300px à la fenetre de droite
+	 $('.panneau_droit').css('height','300px'); 
 
+	// on gère le clic sur le bouton de la fenetre de gauche
 	$('#declenche').click(function(){
 		var saisie=$('#nombre').val();
 		
